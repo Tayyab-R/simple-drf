2cdd52de4d38c3ade1c04fae708ea5fc31d6bf26
Tayyab-R <riaztayyab212@gmail.com>
2024-05-08 15:25:24 +0500

    revert due to issues

--- api/models.py
+++ api/models.py
@@ -12,20 +12,3 @@ class Customer(models.Model):
         return self.name
     
     
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField()
-
-    
-    def __str__(self) -> str:
-        return self.name
-    
-    
-class Inventory(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-
-
-    def __str__(self) -> str:
-        return f"{self.product.name} - Quantity: {self.quantity}"
